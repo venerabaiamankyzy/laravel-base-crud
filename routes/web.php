e2153e543af801583f38b32b71501ede2d085a8f
Venera Baiaman kyzy <venerabayaman@gmail.com>
2023-04-01 22:43:15 +0200

    + TrackController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TrackController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::resource('tracks', TrackController::class);
 
 Route::get('/', function () {
     return view('welcome');
